b0eae7d11d26c76837b32a832fb53c190837ef3d
ketsia0623 <ketsia.donfack@gmail.com>
2024-07-03 10:50:07 -0400

    booleans part of day2

---
+++ day2/11. booleans.py
@@ -0,0 +1,23 @@
+# assigning a boolean value to a variable
+is_active = True 
+print(is_active, "\n")
+
+is_logged_in = False
+print(is_logged_in, "\n")
+
+
+# returning a boolean value
+print(8 == 6)					
+print(4 > 0)
+
+
+# using the bool() function
+print(bool(10))				
+print(bool("well"))	
+
+
+# truthy & falsy
+print(bool(0))				    
+print(bool(False))				    
+print(bool("hello"))		    
+print(bool(None))
