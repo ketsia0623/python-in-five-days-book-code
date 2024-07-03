7d3f9e2df37938260d0d36f2c9a0df5545bdbb75
ketsia0623 <ketsia.donfack@gmail.com>
2024-07-03 15:52:29 -0400

    small format changes

--- day5/8. handling-errors.py
+++ day5/8. handling-errors.py
@@ -2,15 +2,20 @@
 try:
     fave = int(input("Enter your favorite number:"))
     ans = 10 / fave
+
 except ValueError as err:
 	print("Not a valid number")
+
 except ZeroDivisionError as err:
 	print("You can't divide by zero")
+
 else:
 	print(f"Result is {ans}")
+
 finally: 
 	print("Calculation completed")
 	
+
 # raising exceptions
 def checkPositive(number):
 	if number <= 0:
@@ -20,16 +25,21 @@ def checkPositive(number):
 try: 
     num = int(input("Enter a positive number: "))
     checkPositive(num)
+	
 except ValueError as e:
     print("Error: ", e)
+
 
 # custom exceptions
 class customError(Exception):
 	"""My own custom exception class"""
 	pass
+
 def trickyFunction(): 
 	raise customError("Custom error message")
+
 try:
 	trickyFunction()
+
 except customError as e:
 	print("Caught a custom exception: ", e)
