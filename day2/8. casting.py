ec6c44a71172caad2a19393ef02161bf0b9334ab
ketsia0623 <ketsia.donfack@gmail.com>
2024-07-03 10:48:48 -0400

    casting

---
+++ day2/8. casting.py
@@ -0,0 +1,34 @@
+# to integers:
+a = int(4)	
+print(a)	
+
+b = int(4.2)	
+print(b)	
+
+c = int("8")
+print(c)		
+	
+
+# to floats:
+d = float(6)	
+print(d)	
+
+e = float(3.2)	
+print(e)	
+
+f = float("3.2")
+print(f)	
+
+g = float("8")	
+print(g)	
+
+
+# to strings:
+h = str(4)	
+print(h)	
+
+i = str(2.0)	
+print(i)	
+
+j = str("word")
+print(j)		
