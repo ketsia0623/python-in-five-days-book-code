8d90f5398b596ce389790c8bb4867e1adb3ca282
ketsia0623 <ketsia.donfack@gmail.com>
2024-07-04 10:53:48 -0400

    maximus

---
+++ how-to-in-python/5. max-of-list.py
@@ -0,0 +1,12 @@
+num_list = [8, 5, 3, 2, 1, 9, 6, 4, 456, 67, 456, 452, 43, 35, 9]
+
+num_list.sort()              # using the sort() method, sm -> big
+
+print(num_list[-1])          # gets the max
+
+print(num_list[0])           # will give the min
+
+# simple version
+print(max(num_list))         
+
+print(min(num_list))  
