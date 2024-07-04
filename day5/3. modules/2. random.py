2a2b1f0eee82d343f1f8e5e59c0cff362a0f24c9
ketsia0623 <ketsia.donfack@gmail.com>
2024-07-04 12:37:11 -0400

    randyyyy

---
+++ day5/3. modules/2. random.py
@@ -0,0 +1,10 @@
+import random 
+
+# Generating random numbers 
+print(random.randint(1, 10))  
+print(random.random()) 	     
+
+# Shuffling a list 
+my_list = [1, 2, 3, 4, 5] 
+random.shuffle(my_list) 
+print(my_list) 	
